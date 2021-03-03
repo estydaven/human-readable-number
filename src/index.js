60b982263184407fdef03c9e37cd621f4174a0c9
estydaven <esetnod1111@gmail.com>
2021-03-03 17:49:14 +0300

    fix: fix human readable number solution

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-//let number = 999;
 module.exports = function toReadable (number) {
-  let simpleNumbers = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];     
-  let tenNumbers = ['', '', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety', 'one hundred'];
+  let simpleNumbers = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty'];     
+  let tenNumbers = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety', 'one hundred'];
   let result = '';
   let ten = tenNumbers[Math.trunc(number / 10)];
   let hundred = simpleNumbers[Math.trunc(number / 100)];
-  let hundredTen = simpleNumbers[number % 100];
   let num = simpleNumbers[number % 10];
+  let hundredTen = simpleNumbers[number % 100];  
   let leftTen = number % 100;
   let bigHundred = tenNumbers[Math.trunc(leftTen / 10)];
+  let string = number.toString();
   
   if (number === 0){
     return result = 'zero';
@@ -16,48 +16,22 @@ module.exports = function toReadable (number) {
   if (number <= 20){
     return result = simpleNumbers[number];
   }
-  if (number >= 20 && number <= 100){
-    return result = ten + " " + num;
+  if (number > 20 && number <= 100){    
+    if (string[1] === 0){
+      result = ten;
+      return result.trim();
+    } 
+    if (string[0] !== 0) {
+      result = ten + " " + num;
+      return result.trim();
+    }    
   }
   if (number > 100 && leftTen < 20){
-    return result = hundred + ' hundred ' + hundredTen;
+    result = hundred + ' hundred ' + hundredTen;
+    return result.trim();
   }
   if (number > 100 && leftTen >= 20){
-    return result = hundred + ' hundred ' + bigHundred + ' ' + num;
-  }
-  /*let result = '';
-  let string = number.toString();
-  let keysNums = Object.keys(nums);
-  let keysNumbers = Object.keys(numbers);
-  let keysTens = Object.keys(tens);
-
-  if (number === 0){
-    console.log(nums[0]);
-  }
-
-    if (string.length = 1){
-      for (let i = 0; i < keysNums.length; i++){
-          if(string === keysNums[i]){
-            console.log(result = nums[keysNums[i]]);     
-          }
-      }
-    }
- 
-    if (string.length = 2 && string[0] == 1){
-      for (let i = 0; i < keysNumbers.length; i++){
-          if(string === keysNumbers[i]){
-            console.log(result = numbers[keysNumbers[i]]);
-          }
-      }
-    }
-  
-    let leftover = string[0];    
-    let digit = string[1];
-    for (let i = 0; i < keysTens.length; i++){
-      if (leftover === keysTens[i] || digit === keysNums[i]){        
-        console.log(result = (tens[keysTens[i]]) + " " + (nums[keysNums[i]])); 
-      }
-    }*/
-    
-    
+    result = hundred + ' hundred ' + bigHundred + ' ' + num;
+    return result.trim();
+  }  
 }
